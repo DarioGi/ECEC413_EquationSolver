8a28af56916f21479d1962a96081bfc264046cbf
Darius Remeika <Rdariux@gmail.com>
2015-12-05 20:11:57 -0500

    Complete.

--- src/source code/solver.cu
+++ src/source code/solver.cu
@@ -11,8 +11,10 @@ Date: 11/25/2015
 #include <stdlib.h>
 #include <math.h>
 #include "grid.h" // This file defines the grid data structure
+#include <sys/time.h>
+#include <limits.h>
 
-// includes, kernels
+texture<float> tElements;
 #include "solver_kernel.cu"
 
 extern "C" void compute_gold(GRID_STRUCT *);
@@ -77,12 +79,12 @@ void compute_grid_differences(GRID_STRUCT *grid_1, GRID_STRUCT *grid_2)
 
 /* This function creates a grid of random floating point values bounded by UPPER_BOUND_ON_GRID_VALUE */
 void 
-create_grids(GRID_STRUCT *grid_for_cpu, GRID_STRUCT *grid_for_gpu)
+create_grids(GRID_STRUCT *grid_for_cpu, GRID_STRUCT *grid_for_gpu, GRID_STRUCT *grid_for_gpu_text)
 {
 	printf("Creating a grid of dimension %d x %d. \n", grid_for_cpu->dimension, grid_for_cpu->dimension);
 	grid_for_cpu->element = (float *)malloc(sizeof(float) * grid_for_cpu->num_elements);
 	grid_for_gpu->element = (float *)malloc(sizeof(float) * grid_for_gpu->num_elements);
-
+	grid_for_gpu_text->element = (float *)malloc(sizeof(float) * grid_for_gpu_text->num_elements);
 
 	srand((unsigned)time(NULL)); // Seed the the random number generator 
 	
@@ -91,38 +93,138 @@ create_grids(GRID_STRUCT *grid_for_cpu, GRID_STRUCT *grid_for_gpu)
 		for(int j = 0; j < grid_for_cpu->dimension; j++){
 			val =  ((float)rand()/(float)RAND_MAX) * UPPER_BOUND_ON_GRID_VALUE; // Obtain a random value
 			grid_for_cpu->element[i * grid_for_cpu->dimension + j] = val; 	
-			grid_for_gpu->element[i * grid_for_gpu->dimension + j] = val; 				
+			grid_for_gpu->element[i * grid_for_gpu->dimension + j] = val; 
+			grid_for_gpu_text->element[i * grid_for_gpu_text->dimension + j] = val; 			
 		}
 }
 
 
 /* Edit this function skeleton to solve the equation on the device. Store the results back in the my_grid->element data structure for comparison with the CPU result. */
-void 
-compute_on_device(GRID_STRUCT *my_grid)
+void compute_on_device(GRID_STRUCT *my_grid)
 {
+	float *dElements = NULL;
+	float *dDiff = NULL;
+	
+	cudaMalloc((void**)&dElements, my_grid->num_elements * sizeof(float));
+	cudaMemcpy(dElements, my_grid->element, my_grid->num_elements * sizeof(float), cudaMemcpyHostToDevice);
+
+	cudaMalloc((void**)&dDiff, sizeof(float));
+	cudaMemset(dDiff, 0, sizeof(float));
+	
+	int *mutex = NULL;
+    cudaMalloc((void **)&mutex, sizeof(int));
+    cudaMemset(mutex, 0, sizeof(int));
+
+	dim3 thread_block(TILE_SIZE, TILE_SIZE, 1);
+	dim3 grid(1,(my_grid->dimension) / TILE_SIZE);
+
+	float temp = INT_MAX;
+    struct timeval start, stop;
+    gettimeofday(&start, NULL);
+	unsigned int count = 0;
+	
+	while ( temp / (my_grid->dimension * my_grid->dimension) >= TOLERANCE )
+	{
+		cudaMemset(dDiff, 0, sizeof(float));
+		solver_kernel_naive<<<grid, thread_block>>>(dElements, dDiff, mutex);
+		cudaThreadSynchronize();
+		cudaError_t err = cudaGetLastError();
+		if ( cudaSuccess != err ) 
+		{
+			fprintf(stderr, "Kernel execution failed: %s.\n", cudaGetErrorString(err));
+			return;
+		}
+		cudaMemcpy(&temp, dDiff, sizeof(float), cudaMemcpyDeviceToHost);
+		count++;
+	}
+
+	gettimeofday(&stop, NULL);
+    printf("Iterations: %d, GPU Global Memory runtime: %0.5fs.\n", count, (float)(stop.tv_sec - start.tv_sec + (stop.tv_usec - start.tv_usec)/(float)1000000));
+
+	cudaMemcpy(my_grid->element, dElements, my_grid->num_elements * sizeof(float), cudaMemcpyDeviceToHost);
+
+	cudaFree(dElements);
+	cudaFree(dDiff);
+}
+
+// Using texture
+void compute_on_device_text(GRID_STRUCT *my_grid)
+{
+	float *dElements = NULL;
+	float *dDiff = NULL;
+	
+	cudaMalloc((void**)&dElements, my_grid->num_elements * sizeof(float));
+	cudaMemcpy(dElements, my_grid->element, my_grid->num_elements * sizeof(float), cudaMemcpyHostToDevice);
+
+	cudaMalloc((void**)&dDiff, sizeof(float));
+	cudaMemset(dDiff, 0, sizeof(float));
+	
+	cudaBindTexture(NULL, tElements, dElements, my_grid->num_elements * sizeof(float));
+	int *mutex = NULL;
+    cudaMalloc((void **)&mutex, sizeof(int));
+    cudaMemset(mutex, 0, sizeof(int));
+
+	dim3 thread_block(TILE_SIZE, TILE_SIZE, 1);
+	dim3 grid(1,(my_grid->dimension) / TILE_SIZE);
+
+	float temp = INT_MAX;
+    struct timeval start, stop;
+    gettimeofday(&start, NULL);
+	unsigned int count = 0;
+	
+	while ( temp / (my_grid->dimension * my_grid->dimension) >= TOLERANCE )
+	{
+		cudaMemset(dDiff, 0, sizeof(float));
+		solver_kernel_optimized<<<grid, thread_block>>>(dElements, dDiff, mutex);
+		cudaThreadSynchronize();
+		cudaError_t err = cudaGetLastError();
+		if ( cudaSuccess != err ) 
+		{
+			fprintf(stderr, "Kernel execution failed: %s.\n", cudaGetErrorString(err));
+			return;
+		}
+		cudaMemcpy(&temp, dDiff, sizeof(float), cudaMemcpyDeviceToHost);
+		count++;
+	}
+
+	gettimeofday(&stop, NULL);
+    printf("Iterations: %d, GPU Textured Memory runtime: %0.5fs.\n", count, (float)(stop.tv_sec - start.tv_sec + (stop.tv_usec - start.tv_usec)/(float)1000000));
+
+	cudaMemcpy(my_grid->element, dElements, my_grid->num_elements * sizeof(float), cudaMemcpyDeviceToHost);
+
+	cudaFree(dElements);
+	cudaFree(dDiff);
 }
 
 /* The main function */
-int 
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {	
 	/* Generate the grid */
 	GRID_STRUCT *grid_for_cpu = (GRID_STRUCT *)malloc(sizeof(GRID_STRUCT)); // The grid data structure
 	GRID_STRUCT *grid_for_gpu = (GRID_STRUCT *)malloc(sizeof(GRID_STRUCT)); // The grid data structure
-
+	GRID_STRUCT *grid_for_gpu_text = (GRID_STRUCT *)malloc(sizeof(GRID_STRUCT)); // The grid data structure
+	
 	grid_for_cpu->dimension = GRID_DIMENSION;
 	grid_for_cpu->num_elements = grid_for_cpu->dimension * grid_for_cpu->dimension;
 	grid_for_gpu->dimension = GRID_DIMENSION;
 	grid_for_gpu->num_elements = grid_for_gpu->dimension * grid_for_gpu->dimension;
+	grid_for_gpu_text->dimension = GRID_DIMENSION;
+	grid_for_gpu_text->num_elements = grid_for_gpu_text->dimension * grid_for_gpu_text->dimension;
 
- 	create_grids(grid_for_cpu, grid_for_gpu); // Create the grids and populate them with the same set of random values
+ 	create_grids(grid_for_cpu, grid_for_gpu, grid_for_gpu_text);
 	
 	printf("Using the cpu to solve the grid. \n");
+	struct timeval start, stop;
+    gettimeofday(&start, NULL);
 	compute_gold(grid_for_cpu);  // Use CPU to solve 
-	
+	gettimeofday(&stop, NULL);
+    printf("CPU runtime: %0.5f s. \n", (float)(stop.tv_sec - start.tv_sec + (stop.tv_usec - start.tv_usec)/(float)1000000));
 
 	// Use the GPU to solve the equation
 	compute_on_device(grid_for_gpu);
+	
+	// Use the GPU and textured memory to solve the equation.
+	compute_on_device_text(grid_for_gpu_text);
 	
 	// Print key statistics for the converged values
 	printf("CPU: \n");
@@ -131,8 +233,14 @@ main(int argc, char **argv)
 	printf("GPU: \n");
 	print_statistics(grid_for_gpu);
 	
+	printf("GPU textured: \n");
+	print_statistics(grid_for_gpu_text);
+	
     /* Compute grid differences. */
     compute_grid_differences(grid_for_cpu, grid_for_gpu);
+	
+	printf("Textured version: ");
+	compute_grid_differences(grid_for_cpu, grid_for_gpu_text);
 
 	free((void *)grid_for_cpu->element);	
 	free((void *)grid_for_cpu); // Free the grid data structure 
@@ -140,5 +248,8 @@ main(int argc, char **argv)
 	free((void *)grid_for_gpu->element);	
 	free((void *)grid_for_gpu); // Free the grid data structure 
 
+	free((void *)grid_for_gpu_text->element);	
+	free((void *)grid_for_gpu_text); // Free the grid data structure 
+	
 	exit(0);
 }
--- src/source code/solver_kernel.cu
+++ src/source code/solver_kernel.cu
@@ -1,15 +1,104 @@
 #ifndef _MATRIXMUL_KERNEL_H_
 #define _MATRIXMUL_KERNEL_H_
 
-#include <stdio.h>
-
-__global__ void 
-solver_kernel_naive(){
-
+__device__ void lock(int *mutex)
+{
+	while(atomicCAS(mutex, 0, 1) != 0);
 }
 
-__global__ void 
-solver_kernel_optimized(){
+__device__ void unlock(int *mutex)
+{
+	atomicExch(mutex, 0);
+}
+
+__global__ void solver_kernel_naive(float *element, float* global_diff, int * mutex)
+{
+	__shared__ float tRes[TILE_SIZE * TILE_SIZE];
+
+    int rowNumber = blockDim.y * blockIdx.y + threadIdx.y;
+    int columnNumber = threadIdx.x;
+    int tId = threadIdx.y * TILE_SIZE + threadIdx.x;
+    int index = 0.0f;
+    float temp = 0.0f;
+    float diff = 0.0f;
+
+    for ( int i = columnNumber; i < GRID_DIMENSION; i += TILE_SIZE )
+	{
+        if ( i > 0 && i < GRID_DIMENSION - 1 && rowNumber > 0 && rowNumber < GRID_DIMENSION - 1 )
+		{
+            index = rowNumber * GRID_DIMENSION + i;
+            temp = element[index];
+            element[index] = 0.20 * (element[index] +
+								element[index - GRID_DIMENSION] +
+								element[index + GRID_DIMENSION] +
+								element[index + 1] +
+								element[index - 1]);
+            diff += fabsf(element[index] - temp);
+        }
+    }
+    tRes[tId] = diff;
+    __syncthreads();
+	 
+    unsigned int i = TILE_SIZE * TILE_SIZE / 2;
+	while ( i != 0 )
+	{
+		if ( tId < i ) 
+			tRes[tId] += tRes[tId + i];
+		__syncthreads();
+		i /= 2;
+	}
+
+    if ( tId == 0 ) 
+	{
+        lock(mutex);
+        *global_diff += tRes[0];
+        unlock(mutex);
+    }
+}
+
+__global__ void solver_kernel_optimized(float * element, float * global_diff, int * mutex)
+{
+	__shared__ float tRes[TILE_SIZE * TILE_SIZE];
+	
+    int rowNumber = blockDim.y * blockIdx.y + threadIdx.y;
+    int columnNumber = threadIdx.x;
+    int tId = threadIdx.y * TILE_SIZE + threadIdx.x;
+    int index = 0.0f;
+    float temp = 0.0f;
+    float diff = 0.0f;
+
+    for ( int i = columnNumber; i < GRID_DIMENSION; i += TILE_SIZE )
+	{
+        if ( i > 0 && i < GRID_DIMENSION - 1 && rowNumber > 0 && rowNumber < GRID_DIMENSION - 1 )
+		{
+            index = rowNumber * GRID_DIMENSION + i;
+            temp = element[index];
+            element[index] = 0.20 * (tex1Dfetch(tElements, index) +
+							tex1Dfetch(tElements, index - GRID_DIMENSION) +
+							tex1Dfetch(tElements, index + GRID_DIMENSION) +
+							tex1Dfetch(tElements, index + 1) +
+							tex1Dfetch(tElements, index - 1));
+            diff += fabsf(element[index] - temp);
+        }
+    }
+    tRes[tId] = diff;
+    __syncthreads();
+	 
+    unsigned int i = TILE_SIZE * TILE_SIZE / 2;
+	while ( i != 0 )
+	{
+		if ( tId < i ) 
+			tRes[tId] += tRes[tId + i];
+		__syncthreads();
+		i /= 2;
+	}
+
+    if ( tId == 0 ) 
+	{
+        lock(mutex);
+        *global_diff += tRes[0];
+        unlock(mutex);
+    }
 }
 
 #endif /* _MATRIXMUL_KERNEL_H_ */
